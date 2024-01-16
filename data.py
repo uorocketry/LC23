7f97bfa272e866be8b6944d7f7247d16bfe1f75c
Noah Sprenger <noah.r.sprenger@gmail.com>
2024-01-16 17:06:15 -0500

    IDK

--- data.py
+++ data.py
@@ -13,6 +13,7 @@ df_gps1 = pd.read_csv('new_data/gps1.csv')
 df_imu = df_imu[["accel_x", "accel_y", "accel_z", "temp", "timestamp"]]
 df_air = df_air[["pressure_abs", "alt", "timestamp"]]
 df_vel = df_vel[["vel_down", "timestamp"]]
+df_gps = df_gps1[["latitude", "longitude", "timestamp"]]
 
 # df_imu['timestamp_datetime'] = pd.to_datetime(df_imu['timestamp'])
 # timestamps = df_imu['timestamp_datetime']
@@ -21,10 +22,15 @@ df_vel = df_vel[["vel_down", "timestamp"]]
 df_imu['timestamp'] = df_imu['timestamp'].astype(int)
 df_air['timestamp'] = df_air['timestamp'].astype(int)
 df_vel['timestamp'] = df_vel['timestamp'].astype(int)
+# df_gps['timestamp'] = df_gps['timestamp'].astype(int)
+
+df_vel = pd.concat([df_vel,df_gps], axis=0)
+
 
 df_imu.set_index('timestamp', inplace=True)
 df_air.set_index('timestamp', inplace=True)
 df_vel.set_index('timestamp', inplace=True)
+# df_gps.set_index('timestamp', inplace=True)
 df_imu = df_imu.iloc[df_imu.index.get_loc(2880800000):]
 
 df_vel = df_vel.iloc[df_vel.index.get_loc(2879800108):]
@@ -79,7 +85,7 @@ for i in range(1, len(df_imu)):
 
 #     # Update the state transition matrix and control-input model
 #     F[0, 1] = time_diff
-#     B[0, 0] = 0.5 * time_diff**2
+#     B[0, 0] = longitude0.5 * time_diff**2
 
 #     # Predict the next state and covariance
 #     state = np.dot(F, state) + np.dot(B, df_imu.loc[df_imu.index[i], "accel_z"])
@@ -114,7 +120,7 @@ for i in range(1, len(df_imu)):
 
 # Cut off the first 1000 entries 
 
-combined_df = pd.concat([df_imu, df_air, df_vel], ignore_index=False)
+combined_df = pd.concat([df_imu, df_air, df_vel], axis=0)
 # combined_df = pd.concat([df_imu, df_air, df_vel], ignore_index=True)
 # Bunch of garbage at the start. Cut it off.
 # combined_df = combined_df.iloc[1442770471:]
@@ -137,6 +143,8 @@ combined_df.to_csv('combined_data.csv', index=True)
 
 plt.figure(figsize=(10, 6))
 plt.plot(combined_df['vel_down'], label='Velocity Down (z)')
+plt.plot(combined_df['alt'], label='Altitude', color='red')  # Add this line
+plt.plot(combined_df['longitude'], label='Long', color='green')
 plt.xlabel('Time')
 plt.ylabel('Velocity Down (z)')
 plt.title('Rocket Velocity Down Including Initial Velocity')
